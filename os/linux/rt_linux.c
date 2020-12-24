6d7abcc78a72506fca1531a1c412466cc344959f
Denis Sheremet <gitcommitauthor@lxlz.space>
2020-12-24 20:16:12 +0300

    fix read/write

--- os/linux/rt_linux.c
+++ os/linux/rt_linux.c
@@ -1106,22 +1106,12 @@ void RtmpOSFileSeek(RTMP_OS_FD osfd,
 
 int RtmpOSFileRead(RTMP_OS_FD osfd,
 		     char *pDataPtr, int readLen) {
-	/* The object must have a read method */
-	if (osfd->f_op && osfd->f_op->read) {
-		return osfd->f_op->read(osfd, pDataPtr, readLen, &osfd->f_pos);
-	} else {
-		DBGPRINT(RT_DEBUG_ERROR, ("no file read method\n"));
-		return -1;
-	}
+	return kernel_read(osfd, pDataPtr, readLen, &osfd->f_pos);
 }
 
 int RtmpOSFileWrite(RTMP_OS_FD osfd,
 		    char *pDataPtr, int writeLen) {
-	return osfd->f_op->write(osfd,
-				 pDataPtr,
-				 (
-	size_t) writeLen,
-				 &osfd->f_pos);
+	return kernel_write(osfd, pDataPtr, writeLen, &osfd->f_pos);
 }
 
 static inline void __RtmpOSFSInfoChange(OS_FS_INFO * pOSFSInfo,
